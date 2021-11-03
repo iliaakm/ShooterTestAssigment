4370fb7c1bf3b270c5820b46335efa1046301059
iliaakm <iliaakm@gmail.com>
2021-11-03 18:55:56 +0300

    Update Enemy.cs

--- Assets/Scripts/AI/Enemy.cs
+++ Assets/Scripts/AI/Enemy.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class Enemy : MonoBehaviour
 {
+    [Inject(Id = "PlayerTransform")]
+    readonly Transform playerTransform;
+
     [Header("Movement")]
     [SerializeField]
     float movementSpeed;
     [SerializeField]
     Vector2 moveDirection;
+    [SerializeField]
+    float moveDistance;
 
     [Header("Shooting")]
     [SerializeField]
@@ -17,14 +23,16 @@ public class Enemy : MonoBehaviour
     float shootFirePerMin;
     [SerializeField]
     float shootDamage;
-    [SerializeField]
+    [SerializeField, Range(0f, 1f)]
     float shootAccuracy;
 
+    Vector3 startPos;
     float enemyRadius;
 
     private void Start()
     {
         enemyRadius = GetComponent<CapsuleCollider>().radius;
+        startPos = transform.position;
     }
 
     private void FixedUpdate()
@@ -36,19 +44,45 @@ public class Enemy : MonoBehaviour
     {
         RaycastHit hit;
         Vector3 posDelta = Vector3.zero;
+
         Debug.DrawRay(transform.position, transform.TransformDirection(moveDirection), Color.yellow);
+
         if (Physics.Raycast(transform.position, transform.TransformDirection(moveDirection), out hit, enemyRadius + movementSpeed * Time.deltaTime))
         {
-            moveDirection = -moveDirection;
+            ChangeDirection();
             Debug.DrawLine(transform.position, hit.point, Color.blue);
-            print(hit.collider.name);
         }
         else
         {
             posDelta = movementSpeed * Time.deltaTime * moveDirection;
-             
         }
-       
-        transform.position += posDelta;
+
+        Vector3 newPos = transform.position + posDelta;
+        if (Vector3.Distance(startPos, newPos) > moveDistance)
+        {
+            ChangeDirection();
+            newPos = transform.position;
+        }
+
+        transform.position = newPos;
+    }
+
+    void ChangeDirection()
+    {
+        moveDirection = -moveDirection;
+    }
+
+    private void ShootToPlayer()
+    {
+        float hitChance = Random.Range(0f, 1f);
+        if (hitChance > shootAccuracy)
+        {
+            Ray ray = new Ray(this.transform.position, playerTransform.position - this.transform.position);
+            RaycastHit raycastHit;
+            if(Physics.Raycast(ray, out raycastHit, shootRange))
+            {
+
+            }
+        }
     }
 }
