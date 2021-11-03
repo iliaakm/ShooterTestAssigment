7c2317294e8d4f92270cfccc1a05490948ea200e
iliaakm <iliaakm@gmail.com>
2021-11-03 16:38:50 +0300

    Add Cross Hair Scripts

---
+++ Assets/Scripts/UI/CrosshairColors.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/CrosshairColors")]
+public class CrosshairColors : ScriptableObject
+{
+    public Color colorNormal;
+    public Color enemyEnemy;
+
+
+}
