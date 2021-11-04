82908f7be1503a7acd4fa1a8f94937f98771197d
iliaakm <iliaakm@gmail.com>
2021-11-05 00:54:55 +0300

    Add config

---
+++ Assets/Settings/GameConfig.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameConfig
+{
+    public static class Axis
+    {
+        public const string axisFire = "Fire1";
+        public const string axisMoveVertical = "Vertical";
+        public const string AxisMoveHorizontal = "Horizontal";
+        public const string AxisMouseHorizontal = "Mouse X";
+        public const string AxisMouseVertical = "Mouse Y";
+        public const string AxisJump = "Jump";
+    }
+
+    public static class ZenjectConfig
+    {
+        public const string playerTransform = "PlayerTransform";
+        public const string loseTrigger = "LoseTrigger";
+        public const string wonTrigger = "WonTrigger";
+
+    }
+
+    public static class Animation
+    {
+        public const string cannonAnimatorReloading = "Reloading";
+    }
+}
