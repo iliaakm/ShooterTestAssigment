4352ad267f28878e17dcceec6cb7c92ff7d2d10f
iliaakm <iliaakm@gmail.com>
2021-11-05 16:22:29 +0300

    Upd Log Write System

--- Assets/Scripts/Debug/FileLog.cs
+++ Assets/Scripts/Debug/FileLog.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -16,8 +17,15 @@ namespace GameLogSystem
         {
             string filePath = Path.Combine(Application.persistentDataPath, GameConfig.GameLog.fileLogName);
             string message = args.Message + System.Environment.NewLine;
-            File.AppendAllText(filePath, message);
-            Debug.Log($"Log to {filePath}");
+            try
+            {
+                File.AppendAllText(filePath, message);
+                Debug.Log($"Log to {filePath}");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Log Write Error in {filePath} {ex.Message}");
+            }
         }
     }
 }
